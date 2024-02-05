a8aabf85ca685465a7aae40647e705f2974ab645
Farshad Asadpour <asadpoor.f@gmail.com>
2024-02-05 21:54:56 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-x=1
-y=2
-print(f'x+y: {x+y}')
-print(f'x*y: {x*y}')
-print(f'x/y: {x/y}')
-print(f'x%y: {x%y}')
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nginx-deployment
+spec:
+  selector:
+    matchLabels:
+      app: nginx
+  replicas: 2 # tells deployment to run 2 pods matching the template
+  template:
+    metadata:
+      labels:
+        app: nginx
+    spec:
+      containers:
+      - name: nginx
+        image: nginx:1.14.2
+        ports:
+        - containerPort: 80
